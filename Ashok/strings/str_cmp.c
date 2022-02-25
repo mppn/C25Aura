409b3da1ffaf4554b029aed3c1756c4048703c2d
Ashok-kumar107 <99678811+Ashok-kumar107@users.noreply.github.com>
2022-02-25 15:53:36 +0530

    Update str_cmp.c

--- Ashok/strings/str_cmp.c
+++ Ashok/strings/str_cmp.c
@@ -4,7 +4,7 @@ char str_cmp(char *s1,char *s2)
         int i;
         for(i=0;s1[i]!='\0' ||  s2[i]!='\0';i++)
         {
-                printf("\n %d\n",i);
+               
                 if(s1[i]==s2[i])
                 {
 
