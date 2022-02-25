390ce532d2b1fe496de72e7b5da56b5a84cfd2a8
mppn <99004567+mppn@users.noreply.github.com>
2022-02-25 18:35:26 +0530

    Update swapevenodd.c

--- Mamatha/bitmanipulation/swapevenodd.c
+++ Mamatha/bitmanipulation/swapevenodd.c
@@ -1,11 +1,10 @@
 int swapevenoddbits(int n)
 {
-  int n= 25;
-   int even= 25&0xAA;
-     int odd=25&0x55;
- even_bits >>= 1;  
-    odd_bits <<= 1;
-    return (even_bits | odd_bits); 
+   int even= n&0xAA;
+     int odd=n&0x55;
+ even >>1;  
+    odd << 1;
+    return (even| odd); 
 }
 int main()
 {
