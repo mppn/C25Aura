067221ac9fcf9570d5f7c40636226a6171cce952
pawankalayan6899 <99861196+pawankalayan6899@users.noreply.github.com>
2022-02-25 17:04:04 +0530

    Create minandmaxfor1bit.c

---
+++ Pavan/pointers&strings/minandmaxfor1bit.c
@@ -0,0 +1,7 @@
+ A bit has a single binary value, either 0 or 1 //  1 bit = 2 or 2 to the power of 1 = 2
+
+1 = min is 0 and max is 1
+2 = min is 0 and max is 3
+3 = min is 0 and max is 7
+5 = min is 0 and max is 31
+8 = min is 0 and max is 255
