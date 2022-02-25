936d9b8c69874a3afd57f12819b793a2d0792f4c
navathakumari <96461316+navathakumari@users.noreply.github.com>
2022-02-25 17:36:17 +0530

    Update string_tolower.c

--- navatha/strings/string_tolower.c
+++ navatha/strings/string_tolower.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-void strcopy(char[],char[]);
+void str_tolower(char[],char[]);
 
 int main()
 {
     int ind;
     char s1[120]="Ridenext";
     char s2[120];
-    strcopy(s1,s2);
+    str_tolower(s1,s2);
     printf("\n%s",s2);
     printf("\n");
     return 0;
 }
 
-void strcopy(char *x, char *y)
+void str_tolower(char *x, char *y)
 {
      int i=0;
      while(x[i]!='\0')
