eff2abf1d247eb04c961cc9a32fa1a90d1e2b907
Ashok-kumar107 <99678811+Ashok-kumar107@users.noreply.github.com>
2022-02-25 23:04:55 +0530

    Create Program_3.10.c

---
+++ Ashok/Beginners/Program_3.10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+        int i,j,n,x;
+        n=5;
+        x=5;
+        for(i=1;i<=n;i++)
+         {
+                 for(j=1;j<=x;j++)
+                 {
+                         printf("%d",j);
+                }
+                 x--;
+                 printf("\n");
+         }
+
+}
