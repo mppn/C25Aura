bdbe86554ab32f6e4e70645a93c2ff09cce6617e
pawankalayan6899 <99861196+pawankalayan6899@users.noreply.github.com>
2022-02-25 16:49:27 +0530

    Create 7.2.c

---
+++ Pavan/pointers&strings/7.2.c
@@ -0,0 +1,7 @@
+minimum bit we can store is 1 ( 0 or 1 )
+  
+  
+maximum
+
+111 1111(in binary) = 127(in decimal) ; for a signed integer
+1111 1111(in binary) = 255(in decimal) ; for an unsigned integer
