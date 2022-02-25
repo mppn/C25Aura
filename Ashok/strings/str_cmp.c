307f2fb6c2d47d46d9ad172adfa4dc7fae4dc640
Ashok-kumar107 <99678811+Ashok-kumar107@users.noreply.github.com>
2022-02-25 15:48:19 +0530

    Create str_cmp.c

---
+++ Ashok/strings/str_cmp.c
@@ -0,0 +1,20 @@
+char str_cmp(char *s1,char *s2)
+{
+        int x;
+        int i;
+        for(i=0;s1[i]!='\0' ||  s2[i]!='\0';i++)
+        {
+                printf("\n %d\n",i);
+                if(s1[i]==s2[i])
+                {
+
+                  x=1;
+                }
+                else
+                {
+                        return 0;
+                break;
+                }
+        }
+        return x;
+}
