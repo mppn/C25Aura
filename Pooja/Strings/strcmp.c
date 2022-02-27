207f88772d3e56128c8c56b422b483c131d5b55c
Poojanaik-25 <99654599+Poojanaik-25@users.noreply.github.com>
2022-02-27 19:08:35 +0530

    Create strcmp.c

---
+++ Pooja/Strings/strcmp.c
@@ -0,0 +1,18 @@
+int Strcmp(char *x,char *y)
+{
+   int i=0,t;
+    while((x[i]!='\0')||(y[i]!='\0'))
+    {
+        if(x[i]==y[i])
+        { 
+           i++;
+           t=1;
+        }
+        else
+        {
+           break;
+           t=0;
+        }
+    }
+    return t;
+}
