8b3cede4251457a65ad6f9072b1dba3935f281ef
pawankalayan6899 <99861196+pawankalayan6899@users.noreply.github.com>
2022-02-28 15:59:25 +0530

    Update Fibonacci.c

--- Pavan/Beginners_mustdo/Fibonacci.c
+++ Pavan/Beginners_mustdo/Fibonacci.c
@@ -1,1 +1,16 @@
+int main()
+{
+   int a=0, b=1, num, c, sum=0;
 
+   printf("Enter number of terms: ");
+
+   for(int i=0; i<num; i++)
+   {
+      sum += a;
+      c = a + b; 
+      a = b;
+      b = c;
+   }
+
+   printf("Sum = %d", sum);
+}
