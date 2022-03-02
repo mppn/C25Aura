ef51b274d0351baddb52d87790a000780053701b
Poojanaik-25 <99654599+Poojanaik-25@users.noreply.github.com>
2022-03-02 21:14:56 +0530

    Update Sum.c

--- Pooja/Beginners_mustdo/Sum.c
+++ Pooja/Beginners_mustdo/Sum.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main() 
+{
+    int a=426,c;
+    int sum=0;
+    while(a>0)
+    {
+        c=a%10;
+        sum=sum+c;
+        a=a/10;
+    }
+    printf("Sum of digits is %d\n",sum);
+}
