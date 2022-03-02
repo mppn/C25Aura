9c641f41a4ccf1a10f1e3bb9156f5855ad0b0fc8
pawankalayan6899 <99861196+pawankalayan6899@users.noreply.github.com>
2022-03-02 10:37:20 +0530

    Create nfact.c

---
+++ Pavan/Beginnersmustdo/nfact.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fact(int);
+int main()
+{
+    int i,n=5;
+    for(i=1;i<=n;i++)
+    {
+        printf("%d,",fact(i));
+    }
+    printf("\n");
+    return 0;
+}
+
+int fact(int n)
+{
+    int i,f=1;
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+    return f;
+}
