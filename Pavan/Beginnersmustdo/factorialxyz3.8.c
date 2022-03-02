b54d9b01609e46dd84952c0d85aded79d039f2df
pawankalayan6899 <99861196+pawankalayan6899@users.noreply.github.com>
2022-03-02 11:41:15 +0530

    Create factorialxyz3.8.c

---
+++ Pavan/Beginnersmustdo/factorialxyz3.8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int fact(int);
+int main()
+{
+    int i,x=3,y=4,n=5;
+    float d,s=0;
+    for(i=1;i<=n;i++)
+    {
+        d=(float)fact(x)/fact(y);
+        s=s+d;
+        x++;
+        y++;
+    }
+    
+    printf("the sum of factorials of %d numbers is %f",n,s);
+    printf("\n");
+    return 0;
+}
+
+int fact(int n)
+{
+    int i,f=1;
+    for(i=1;i<=n;i++)
+    {
+        f=f*i;
+    }
+    return f;
+}
