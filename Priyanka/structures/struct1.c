90a134d2f0e668a2a77bacb6389376f1282e7c82
ridenext-p <99651067+ridenext-p@users.noreply.github.com>
2022-03-06 12:10:56 +0530

    Update struct1.c

--- Priyanka/structures/struct1.c
+++ Priyanka/structures/struct1.c
@@ -1,16 +1,19 @@
-struct Student {
-        int id;
-        int age;
-}s1;
-
+struct Student
+{
+    int id;
+    short age;
+    char name[5];
+}student1;
 int main()
 {
-    
-    s1.id=234;
-    s1.age=7;
-    printf("%ld",sizeof(s1));
-    printf("%ld",sizeof(s1.id));
-    printf("%ld",sizeof(s1.age));
-    printf("%ld",&s1.id);
-    printf("%ld",&s1.age);
+    student1.id=2;
+    student1.age=21;
+    strcpy(student1.name,"priya");
+    printf("%p\n",&student1.id);
+    printf("%p\n",&student1.name);
+    printf("%p\n",&student1.age);
+    printf("%ld\n",sizeof(student1.id));
+    printf("%ld\n",sizeof(student1.name));
+    printf("%ld\n",sizeof(student1.age));
+    return 0;
 }
