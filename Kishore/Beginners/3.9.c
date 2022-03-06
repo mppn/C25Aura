4b75b9c3650f1d1103b0db7a781fd00f65967d2c
kishore-06 <99721087+kishore-06@users.noreply.github.com>
2022-03-06 22:40:31 +0530

    Create 3.9.c

---
+++ Kishore/Beginners/3.9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ 
+ void main()
+ {
+       
+       int i,j,n;
+       n=5;
+       i=1;
+       for(i=1;i<=5;i++)
+        {
+            printf("%d",i);
+        }
+       for(j=4;j>=1;j--)
+       {
+           printf("%d",j);
+       }
+ }   
