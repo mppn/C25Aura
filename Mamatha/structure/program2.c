b0f8b44b0d0801d95757b843725e34df6a287f89
mppn <99004567+mppn@users.noreply.github.com>
2022-03-07 06:09:11 +0530

    Update program2.c

--- Mamatha/structure/program2.c
+++ Mamatha/structure/program2.c
@@ -1,1 +1,15 @@
 
+struct Student {
+        int id;
+        int age;
+}s1;
+
+int main()
+{
+
+    s1.id=25;
+    s1.age=8;
+    printf("%ld",sizeof(s1));
+    printf("%ld",sizeof(s1.id));
+    printf("%ld",sizeof(s1.age));
+}
