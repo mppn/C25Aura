69fb371d600821091cf7a3a84dcb08d8f1872b7c
navathakumari <96461316+navathakumari@users.noreply.github.com>
2022-03-08 00:46:11 +0530

    Update index.c

--- navatha/pointers&strings/index.c
+++ navatha/pointers&strings/index.c
@@ -1,14 +1,12 @@
 char *Index(char *x, int ch)
 {
-   char *i=0;
-    while(1)
+  while(1)
     {
         if(*x==(char)ch)
         {
             break;
         }
         x++;
-        i++;
-    }
-    return i;
+  }
+    return x;
 }
